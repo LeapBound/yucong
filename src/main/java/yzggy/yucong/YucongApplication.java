47741eab2bc1501ad2c19bd1064f73e10aa380c8
Fred Goo <fredera92@gmail.com>
2023-06-26 17:49:11 +0800

    初始化java版本

---
+++ src/main/java/yzggy/yucong/YucongApplication.java
@@ -0,0 +1,13 @@
+package yzggy.yucong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YucongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YucongApplication.class, args);
+    }
+
+}
