a2559f59626c23dd99fa90861a0c4ac516e17b0b
yamath <yaozhigang02@geexfinance.com>
2023-12-21 11:10:11 +0800

    [fix] fix deploy-page.yml

---
+++ pages/docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: 'LeapBound',
+  description: 'in Leaps and Bounds',
+  base: '/yucong/',
+})
