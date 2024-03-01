a2576cfc44165cc424c4b5b0158f2c40bfdfac4b
Fred Goo <fredera92@gmail.com>
2024-03-01 17:53:21 +0800

    aio add process

--- examples/yc-process-example/src/main/java/com/github/leapbound/YcCamundaExampleApplication.java
+++ examples/yc-process-example/src/main/java/com/github/leapbound/YcCamundaExampleApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- *
- *
  * @author tangxu
  * @since 2024/3/1 15:31
  */
