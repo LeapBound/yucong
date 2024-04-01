8d0f1b6719f4609ce6f3c9d1de9e6174c5f00625
GEEX1366 <tangxu@geexfinace.com>
2024-04-01 11:44:16 +0800

    add table:yc_function_task

---
+++ yc-action-server/src/main/java/com/github/leapbound/yc/action/model/vo/request/FunctionTaskRequest.java
@@ -0,0 +1,61 @@
+package com.github.leapbound.yc.action.model.vo.request;
+
+import com.alibaba.fastjson2.JSON;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+/**
+ * function <-> task service
+ *
+ * @author tangxu
+ * @since 2024/3/29 17:33
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class FunctionTaskRequest extends BaseRequest {
+
+    private Integer id;
+
+    /**
+     * 方法名
+     */
+    private String functionName;
+
+    /**
+     *  task name
+     */
+    private String taskName;
+
+    /**
+     * 话术
+     */
+    private List<Script> script;
+
+    public String scriptJson() {
+        return JSON.toJSONString(this.script);
+    }
+
+    /**
+     * 话术规则
+     */
+    @Data
+    public static class Script {
+
+        /**
+         * 类型：
+         * 执行成功/执行失败（外部接口）/调用超时/异常失败（本地异常）
+         * SUCCESS/FAIL/TIMEOUT/EXCEPTION
+         */
+        private String type;
+
+        /**
+         * 对应的提示信息
+         */
+        private String message;
+
+
+    }
+
+}
