ca82e57b8957376fc4fb73787cdaeb05591b9fbb
yamath <yaozhigang02@geexfinance.com>
2024-04-02 15:34:18 +0800

    feature | yc-hub transfer to yc-action #63

--- yc-action-server/src/main/resources/scripts/cashloan/CashLoan.groovy
+++ yc-action-server/src/main/resources/scripts/cashloan/CashLoan.groovy
@@ -4,7 +4,7 @@ import cn.hutool.core.util.StrUtil
 import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.JSONObject
 import groovy.transform.Field
-import yzggy.yucong.action.func.groovy.RestClient
+import com.github.leapbound.yc.action.func.groovy.RestClient
 
 import java.math.RoundingMode
 
--- yc-action-server/src/main/resources/scripts/order/Order.groovy
+++ yc-action-server/src/main/resources/scripts/order/Order.groovy
@@ -5,7 +5,7 @@ import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.JSONArray
 import com.alibaba.fastjson.JSONObject
 import groovy.transform.Field
-import yzggy.yucong.action.func.groovy.RestClient
+import com.github.leapbound.yc.action.func.groovy.RestClient
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
